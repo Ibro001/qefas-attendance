cd7f7a161bb76f0ff97960b2bf032361d1ce4232
ibro001 <ibrahimmoses56@gmail.com>
2024-02-22 16:00:59 +0100

    init commit

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use("/", express.static("../frontend/dist"));
-app.get('../frontend/index.html');
+app.get("/", express.static("../frontend/dist"));
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
