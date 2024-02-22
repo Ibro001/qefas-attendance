7c51d1acc16da5f4e0fa663193c76cc3daffcbb2
ibro001 <ibrahimmoses56@gmail.com>
2024-02-22 14:43:33 +0100

    config static site

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+app.use("/", express.static("../frontend/dist"));
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
